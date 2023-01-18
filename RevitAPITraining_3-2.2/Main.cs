9ec8c43dfcf5d6983115540b4cc420113c5771f5
Дмитрий Изох <izokh_dv@mail.ru>
2023-01-18 23:18:29 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITraining_3-2.2/Main.cs
@@ -0,0 +1,39 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITraining_3_2._2
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        
+        public string massage = "Количество труб на текущем виде";
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            List<FamilyInstance> fInstances = new FilteredElementCollector(doc, doc.ActiveView.Id)
+            .OfCategory(BuiltInCategory.OST_PipeCurves)
+            .WhereElementIsNotElementType()
+            .Cast<FamilyInstance>()
+            .ToList();
+            
+            TaskDialog.Show(massage, fInstances.Count.ToString());
+            return Result.Succeeded;
+        }
+
+    }   
+
+}
+
+
